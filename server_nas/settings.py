cc62ee68268f414d88a6a2c09393f66e70cb780e
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-06-21 16:12:32 +0700

    baru

--- server_nas/settings.py
+++ server_nas/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,13 +25,13 @@ SECRET_KEY = 'django-insecure-e+z$nr#fybc9*y7wg%940i82o!+!e!q%6k47t6j1u2b3lu6nmm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.0.20', 'localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    'django.contArib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
