018b83d3a369c8386ecedcce2c0da439844bdcaf
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-06-21 14:33:30 +0700

    first commit

---
+++ server_nas/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for server_nas project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/add_folder/', views.add_folder, name='add_folder'),
+]
+
+
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     # path('api/update-containers/', views.update_containers),
+#     # path('api/create-folders/', views.create_folders),
+#     path('api/add-folders/', views.add_folder),
+# ]
+
+
---
+++ server_nas/views.py
@@ -0,0 +1,132 @@
+import subprocess
+import os
+from django.http import JsonResponse
+
+def add_folder(request):
+    folder = request.GET.get('folder')
+    if folder:
+        if os.path.exists(folder):
+            response = {'error': 'Folder already exists.'}
+        else:
+            try:
+                subprocess.run(f'mkdir {folder}', shell=True, check=True)
+                response = {'message': 'Folder created successfully.'}
+            except subprocess.CalledProcessError:
+                response = {'error': 'Failed to create folder.'}
+    else:
+        response = {'error': 'Folder parameter is missing.'}
+    return JsonResponse(response)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import json
+# from django.http import JsonResponse
+# # from .models import Container 
+# from django.views.decorators.csrf import csrf_exempt
+# import subprocess
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# @csrf_exempt
+# def update_containers(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body.decode('utf-8')).get('data')
+#         print(data)  # Periksa nilai data
+#         # Proses data yang diterima dari Django A
+#         # Misalnya, perbarui nilai status_job pada objek Container
+#         updated_values = []
+#         for container_data in data:
+#             container_id = container_data.get('id')
+#             status_job = container_data.get('status_job')
+
+#             # # Lakukan operasi yang sesuai pada objek Container
+#             # container = Container.objects.get(id=container_id)
+#             # container.status_job = status_job
+#             # container.save()
+
+#             # Buat entri yang mengandung nilai terbaru
+#             updated_values.append({
+#                 'id': container_id,
+#                 'status_job': 1
+#             })
+        
+
+#         # Kembalikan nilai yang diperbarui ke Django A
+#         response_data = {
+#             'updated_values': updated_values
+#         }
+
+#         return JsonResponse(response_data)
+#     else:
+#         return JsonResponse({'message': 'Invalid request method.'}, status=400)
+
+
+# @api_view(['POST'])
+# def create_folders(request):
+#     users = request.data.get("users")
+
+#     for user in users:
+#         nim = user.get("nim")
+
+#         # Eksekusi perintah shell untuk membuat folder dengan nama NIM
+#         command = f"mkdir {nim}"
+#         subprocess.run(command, shell=True)
+
+#     return Response({"message": "Folders created successfully."})
+
+# # @csrf_exempt
+# # def update_containers(request):
+# #     if request.method == 'POST':
+# #         data = json.loads(request.body.decode('utf-8')).get('data')
+# #         print(data)  # Periksa nilai data
+# #         # Proses data yang diterima dari Django A
+# #         # Misalnya, perbarui nilai status_job pada objek Container
+# #         updated_values = []
+# #         for container_data in data:
+# #             container_id = container_data.get('id')
+# #             status_job = container_data.get('status_job')
+
+# #             # # # Lakukan operasi yang sesuai pada objek Container
+# #             # container = Container.objects.get(id=container_id)
+# #             # container.status_job = status_job
+# #             # container.save()
+
+# #             # Buat entri yang mengandung nilai terbaru
+# #             updated_values.append({
+# #                 'id': container_id,
+# #                 'status_job': 1
+# #             })
+# #         # Send the updated values to Django 1 for saving
+# #         url = "http://127.0.0.1:8000/api/save-containers/"  # Replace with the URL endpoint of Django 1
+# #         headers = {
+# #             "Content-Type": "application/json"
+# #         }
+# #         payload = {
+# #             "data": updated_values
+# #         }
+# #         json_payload = json.dumps(payload)
+
+# #         response = requests.post(url, data=json_payload, headers=headers)
+# #         if response.status_code == 200:
+# #             return JsonResponse({'message': 'Data processed and sent to Django 1.'})
+# #         else:
+# #             return JsonResponse({'message': 'Failed to send data to Django 1.'}, status=500)
+# #     else:
+# #         return JsonResponse({'message': 'Invalid request method.'}, status=400)
+
+
+
