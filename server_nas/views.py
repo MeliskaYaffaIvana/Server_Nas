1d4b04e6c84d364fffc095372bef96aa38101497
MeliskaYaffaIvana <meliskayaffaivana725myip29@gmail.com>
2023-06-27 15:47:05 +0700

    coba

--- server_nas/views.py
+++ server_nas/views.py
@@ -46,11 +46,11 @@ def run_command_with_sudo(command):
 
 @csrf_exempt
 def add_unix_user(request):
-    password = request.POST.get('password')
+    nim = request.POST.get('nim')
     nim = request.POST.get('nim')
 
     try:
-        command = '/usr/sbin/useradd -p ' + str(password) + ' -m -s /bin/bash -g hosting-users ' + nim
+        command = '/usr/sbin/useradd -p ' + str(nim) + ' -m -s /bin/bash -g hosting-users ' + nim
         run_command_with_sudo(command)
     except CalledProcessError:
         return JsonResponse({'status': 'error', 'message': 'Error adding Unix user'})
